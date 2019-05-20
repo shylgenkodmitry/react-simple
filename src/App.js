58fd4c7c74dcd3f18c1bf75eac2b1d5e01656bd7
Dmitry Shylgenko <shylgenko.dmitry@gmail.com>
2019-05-20 10:16:31 +0300

    Creating simple table component

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import Table from './Table'
+
+class App extends Component {
+    render() {
+        const characters = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            },
+        ]
+
+        return (
+            <div className="container">
+                <Table characterData={characters} />
+            </div>
+        )
+    }
+}
+
+export default App
