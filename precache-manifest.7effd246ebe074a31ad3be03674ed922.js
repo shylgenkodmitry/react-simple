71885ad19d12a3543e21aee903508bbb1dbf33d6
Dmitry Shylgenko <shylgenko.dmitry@gmail.com>
2019-05-21 06:06:28 +0300

    Updates

---
+++ precache-manifest.7effd246ebe074a31ad3be03674ed922.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ad220d947d43d905cf7fbe7a5e4b30",
+    "url": "/react-simple/index.html"
+  },
+  {
+    "revision": "344e6249c85a1174f135",
+    "url": "/react-simple/static/css/main.0259b24e.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-simple/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "344e6249c85a1174f135",
+    "url": "/react-simple/static/js/main.73cdcf8f.chunk.js"
+  },
+  {
+    "revision": "369f15795e106707a53f",
+    "url": "/react-simple/static/js/runtime~main.4c8180bb.js"
+  }
+]);
